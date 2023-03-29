b80ecd410ca3fa2c23078bd380dad60acc29e908
sauravs4h <sauravs4h@gmail.com>
2023-03-29 12:36:17 +0530

    d

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("hello")
+const express= require("express");
+
+const app=express();
+
+
+app.get("/",(req,res)=>{
+    res.send("base api");
+})
+
+
+
+app.listen(8080,()=>{
+    console.log("8080 is working")
+})
